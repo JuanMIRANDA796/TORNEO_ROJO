46c9db18c789d188bc35c500884e1d6ec6307ad1
JuanMIRANDA796 <juan.mirandar@udea.edu.co>
2025-03-02 01:03:00 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,20 +1,36 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-from PIL import Image
-import io
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Configuración inicial del Dashboard
 st.set_page_config(page_title='Torneo_ROJO', layout='wide')
 
 st.title("Información del torneo ⚽")
 
-st.write("Nea esta pagina se crea para dar toda la informción del torneo que se jugara el proximo Viernes, la idea es que la información de la pagina se vaya actualizando a medida que los equipos de vayan inscribiendo y a medida en que el torneo este transcurriendo")
+st.write("Nea esta página se crea para dar toda la información del torneo que se jugará el próximo Viernes, la idea es que la información de la página se vaya actualizando a medida que los equipos se vayan inscribiendo y a medida en que el torneo esté transcurriendo.")
 
 # Menú lateral para navegación
 st.sidebar.title("📌 Menú de Opciones")
-option = st.sidebar.radio("Selecciona una opción", ["Equipos ", "Tabla de posiciones", "Esquema del Torneo", "Premiación", "Inscripción"])
+option = st.sidebar.radio("Selecciona una opción", ["Equipos", "Tabla de posiciones", "Esquema del Torneo", "Premiación", "Inscripción"])
 
+# Configuración de las credenciales y acceso a Google Sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('ruta_al_archivo_json_de_credenciales.json', scope)
+client = gspread.authorize(creds)
 
-  
+# ID del documento de Google Sheets
+sheet_id = '1ibU57_4hTSA79oE-B8M9Se5f7A0uHi9nr_VJtPmcpRE'
+sheet_name = 'INSCRIPCIÓN'
+
+def load_google_sheet(sheet_id, sheet_name):
+    sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+    return df
+
+if option == "Equipos":
+    df = load_google_sheet(sheet_id, sheet_name)
+    st.write(df)
+
+# Añade las demás opciones aquí...
