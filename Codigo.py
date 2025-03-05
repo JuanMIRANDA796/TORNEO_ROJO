737010c627a66c0ed1de1240b4cfb58ee5fec639
JuanMIRANDA796 <juan.mirandar@udea.edu.co>
2025-03-05 09:49:00 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -38,7 +38,8 @@ option = st.sidebar.radio("Selecciona una opción", ["Equipos", "Tabla de posici
 # Mostrar la tabla de equipos cuando se selecciona la opción "Equipos"
 if option == "Equipos":
     st.header("Equipos Participantes")
-    # Leer el archivo CSV
-    df = pd.read_csv('INSCRIPCIÓN (respuestas) - R_1.csv')
+    # Leer el archivo CSV desde la URL
+    url = 'https://raw.githubusercontent.com/JuanMIRANDA796/TORNEO_ROJO/refs/heads/main/INSCRIPCIÓN%20(respuestas)%20-%20R_1.csv'
+    df = pd.read_csv(url)
     # Mostrar la tabla en el Dashboard
     st.dataframe(df)
