f8daa97cb3829af3b2c6fb0a45fc5bb23a443955
JuanMIRANDA796 <juan.mirandar@udea.edu.co>
2025-03-05 11:25:49 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -5,18 +5,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from datetime import datetime
+from pathlib import Path
 
-#1. Configuración inical de la aplicacion
+#1. Configuración inicial de la aplicación
 
 st.set_page_config(
   page_title = "TORNEO ECONOMICAS ⚽",
   page_icon = " "
 )
 st.title("TORNEO ECONOMICAS 🍀⚽🍀")
-st.sidebar.title("Opciones de  navegación")
+st.sidebar.title("Opciones de navegación")
 
 menu = st.sidebar.radio(
     "Selecciona una opción:",
     ["Inicio", "Equipos", "Inscripción", "Premiación", "Información general"]
 )
 
+# Ruta de la imagen y el archivo de música
+image_path = Path.home() / "Downloads" / "image(4).png"  # Cambia 'nombre_de_tu_imagen.png' por el nombre de tu imagen
+audio_path = Path.home() / "Downloads" / "nombre_de_tu_audio.mp3"   # Cambia 'nombre_de_tu_audio.mp3' por el nombre de tu archivo de audio
+
+#2. Mostrar la imagen y reproducir el audio en la opción de inicio
+if menu == "Inicio":
+    st.header("Bienvenido al Torneo Económicas")
+    st.image(str(image_path), caption="Imagen cargada")  # Muestra la imagen desde la carpeta de descargas
+    st.audio(str(audio_path))  # Reproduce el archivo de audio desde la carpeta de descargas
